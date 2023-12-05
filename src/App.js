9943d88c37e2740c4153145e9f38dc253308fbb1
ajinkya8970 <ajinkya8970@gmail.com>
2023-12-05 09:10:55 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-// import { HashRouter, Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
